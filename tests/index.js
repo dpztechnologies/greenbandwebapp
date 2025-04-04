1a8b08f2c9b097402283640fb44c54e56172228c
PeterMwambi <calebmwambi@gmail.com>
2025-04-04 05:18:56 +0300

    created login form

--- tests/index.js
+++ tests/index.js
@@ -1,44 +1,30 @@
-const { routeProvider } = require('./routes.cjs');
-const fs = require('fs');
-const utilities = require('./utilities.cjs')
-const DB = require('./database.cjs');
-
-
-
+const { routeProvider } = require("./routes.cjs");
+const fs = require("fs");
+const utilities = require("./utilities.cjs");
+const DB = require("./database.cjs");
 
 const app = routeProvider();
 
-
-app.get('/', (req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' })
-    res.end('Welcome to the home page');
-})
-
-app.post('/register', (req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Test Post');
-})
-
-app.get('/home', (req, res) => {
-    fs.readFile(utilities.getFilePath('login'), (err, data) => {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
+app.get("/", (req, res) => {
+    fs.readFile(utilities.getFilePath("login"), (err, data) => {
+        res.writeHead(200, { "Content-Type": "text/html" });
         res.write(data);
         res.end();
-    })
-})
+    });
+});
 
+app.post("/register", (req, res) => {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("Test Post");
+});
 
-app.get('/users', (req, res) => {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
+app.get("/users", (req, res) => {
+    res.writeHead(200, { "Content-Type": "application/json" });
     (async () => {
-        let db = await DB.use().select(['*']).from('system_admins', true);
+        let db = await DB.use().select(["*"]).from("system_admins", true);
         res.write(JSON.stringify(db.getResults()));
-        res.end()
-    })()
-})
+        res.end();
+    })();
+});
 
 app.start();
-
-
-
-
