fc1fa4b29bdc2ea5a4b2fefba59f99c7c2cf6b77
petercaleb <petermwasagua@gmail.com>
2025-04-24 23:19:45 +0300

    exported database tables

--- storage/database/greenbandwebapp.sql
+++ storage/database/greenbandwebapp.sql
@@ -32,7 +32,7 @@ DROP TABLE IF EXISTS `admins`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `admins` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `uid` int(50) NOT NULL,
+  `aid` int(50) NOT NULL,
   `firstname` varchar(500) NOT NULL,
   `lastname` varchar(500) NOT NULL,
   `email` varchar(500) NOT NULL,
@@ -40,10 +40,13 @@ CREATE TABLE `admins` (
   `password` varchar(500) NOT NULL,
   `role` varchar(500) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `uid` (`uid`),
+  UNIQUE KEY `uid` (`aid`),
   UNIQUE KEY `email` (`email`),
-  UNIQUE KEY `phone_no` (`phone_no`)
-) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  UNIQUE KEY `phone_no` (`phone_no`),
+  UNIQUE KEY `aid` (`aid`),
+  UNIQUE KEY `a_id` (`aid`),
+  UNIQUE KEY `aid_2` (`aid`)
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -52,8 +55,36 @@ CREATE TABLE `admins` (
 
 LOCK TABLES `admins` WRITE;
 /*!40000 ALTER TABLE `admins` DISABLE KEYS */;
-INSERT INTO `admins` VALUES (17,10205,'Peter','Mwambi','calebmwambi@gmail.com','0700521998','$2b$12$NJRmBTJjyiiqz5RuNlFKoOs7DV9AFchI9dnrs.ZeYOO8PJemYsR1G','Super Admin'),(18,5837,'Susan','Njeri','sue@gmail.com','0114958431','$2b$12$3XYF44nY3WGoQtp.zzhGCelrKBydvgY5xt.DbnVGxw/vNrx1dH1uK','System Admin'),(19,7974,'Joyce','Wangari','joyce@gmail.com','0723117035','$2b$12$jjQe3aCz0ivWU9Uc7jOcUel52VCmzw/t1iwQAU4OyqH5md.EL4Kuq','System Admin'),(20,6678,'Anthony','Njenga','anthony.njenga@gmail.com','07117231221','$2b$12$hlez3y6Ze9vx7CQBW73SJeLdppwQVHQ8d4ANBIlEbb1.1cLnVlCHW','Super Admin'),(21,2456,'Anthony','Kamau','anthony.kamau@gmail.com','0113277585','$2b$12$vJwP7GQ1U6ytTbjr6eve1egi19mBq3CS3wXluVDzIlBcIoyd4a6GG','System Admin'),(22,7466,'Nancy','Mueni','mueni.nancy@gmail.com','0721227121','$2b$12$YRc.MLw5c6FvECUb7okoCOsDMNExjGq6NUO7vhmuqjoD/ckAWMIj.','System Admin'),(23,4816,'Nicholas','Amani','nicholas.amani@gmail.com','0711221377','$2b$12$naMq6n7CqH341udOxzI58e0QOsR2xODUG3xOQe0njGmd72pggInde','System Admin'),(24,4212,'Morgan','Mwangi','morgan.mwangi@gmail.com','0723117234','$2b$12$ZmrpBsYzWEtaAErACOZ1HOUJ9PnFfsrlwfzKxD1gxiOa9nuVKuy4O','Super Admin'),(25,9408,'Peter','Kamau','kamau.peter@gmail.com','0716231122','$2b$12$FaEy1w1ZwuHOsIzXc0GIUOg1tdmsX0MUy.56q8zmViM5VlhZ9owRS','System Admin'),(26,1806,'Lilian','Mueni','lily@gmail.com','0729771773','$2b$12$9WBYJTKT4WeQ2l2iVTHyJ.9qfdBjhjpbF4/n1LzbJaCJu2Mtb34ku','System Admin'),(27,7009,'Nichiolai','Opiyo','nicholai@gmail.com','0702112837','$2b$12$eeUbsl2A9x1inJ2Ytf6vm.pD7IWnX4WMT4/3EHmK6wbLmdoBrS0gK','System Admin'),(28,6590,'Mary','Njambi','marynjambi@gmail.com','0764127221','$2b$12$rH0smyUi8vzUiB7LI6zerON0a4IwfabFyjee/sLDwuiCyISUeIZTS','Super Admin'),(29,7553,'Annette','Njeri','annette.njeri@gmail.com','0721275380','$2b$12$reujbwgVgFEd5VJIRSgLf.dCvrvuP.BRUs9Z/h4O8WdciybUpq7Vy','System Admin');
+INSERT INTO `admins` VALUES (33,3677,'Peter','Mwambi','calebmwambi@gmail.com','0114958431','$2b$12$LXSlXFRO48zbTIwWIYnR9Om9jGOkCGB2RhBq5Lg8Xxpg6I6KccjDq','Super Admin'),(34,5977,'Caleb','Mwambi','c.mwambi@gmail.com','0700521998','$2b$12$QEzIuWdmrCjtb3rPsbWJYedPZ6Jht9qlqbHmB8Xt35Q2EH.FnaJNq','Super Admin'),(35,1617,'Jared','Mwangi','jmwangi@gmail.com','0714227121','$2b$12$xlMNTJ6MzBQuw2y.70p7UuNMar8.0844sbEgLonnCTDm9.T7KJUH6','System Admin'),(36,6061,'Caleb','Mwasagua','p.mwambi@gmail.com','0721623731','$2b$12$LZfGtv.rQNAO7RIFBu5SVur4yb6qpK4GAJ7o4XjLMhMP22uSyyapi','Super Admin');
 /*!40000 ALTER TABLE `admins` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `admins_activity`
+--
+
+DROP TABLE IF EXISTS `admins_activity`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `admins_activity` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `aid` int(50) NOT NULL,
+  `status` varchar(100) NOT NULL DEFAULT 'Offline',
+  `can_access` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `a_id` (`aid`),
+  UNIQUE KEY `aid` (`aid`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `admins_activity`
+--
+
+LOCK TABLES `admins_activity` WRITE;
+/*!40000 ALTER TABLE `admins_activity` DISABLE KEYS */;
+INSERT INTO `admins_activity` VALUES (3,3677,'Online',0),(4,5977,'Online',0),(5,1617,'Offline',0),(6,6061,'Offline',0);
+/*!40000 ALTER TABLE `admins_activity` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -77,7 +108,7 @@ CREATE TABLE `sessions` (
 
 LOCK TABLES `sessions` WRITE;
 /*!40000 ALTER TABLE `sessions` DISABLE KEYS */;
-INSERT INTO `sessions` VALUES ('8d41dd5594590a687c950d2c92fe3cad6b937c91b9945770c18c5f9c51b0a46a','annette.njeri@gmail.com','2025-04-18 08:49:12'),('bd575bc2b38e583212dd1d0f4d063def3e9b429cd9e08ae71d8b81473981cae7','calebmwambi@gmail.com','2025-04-18 20:56:05');
+INSERT INTO `sessions` VALUES ('18375efa09fe32ed511cefaeaca67badf4b5b6e666df52a238f3d9bc7da03072','c.mwambi@gmail.com','2025-04-24 19:50:50'),('8f2733dcf3d4c530a51caa60d37da82c93b6c0ca39ec9da4af54be48aac0ef92','calebmwambi@gmail.com','2025-04-24 21:05:37'),('daeea950d246984c32d1379f72c84972fb3c10807fe239425b10252ecd606d14','calebmwambi@gmail.com','2025-04-24 19:49:46'),('e2fffdb962102315772d81ff3723a43b4f88678f4c2c2e1616ac232fdcc5b91e','c.mwambi@gmail.com','2025-04-24 18:04:06');
 /*!40000 ALTER TABLE `sessions` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -90,4 +121,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-04-18 21:03:41
+-- Dump completed on 2025-04-24 23:19:03
